cc823663f4ec6b281a51a743e19885e52f9de382
pockypycok <cheophy8@gmail.com>
2026-01-21 15:24:54 +0700

    dd

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -3,11 +3,11 @@ if (isNaN(score) || score < 0 || score > 100) {
 console.log("Invalid Input");
 } else if (score >= 80 && >= 100) {
 console.log("A");
-} else if (score >= 70) {
+} else if (score >= 70 && >= 79) {
 console.log("B");
-} else if (score >= 60) {
+} else if (score >= 60 && >= 69) {
 console.log("C");
-} else if (score >= 50) {
+} else if (score >= 50 && >= 59) {
 console.log("D");
 } else {
 console.log("F");
