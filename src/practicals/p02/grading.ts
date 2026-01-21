f429b4a401cc8a1fa678f89cc8c32e9bf8112244
pockypycok <cheophy8@gmail.com>
2026-01-21 15:40:53 +0700

    ooooo

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,13 +1,13 @@
 const score = Number(process.agrv[2]);
 if (isNaN(score) || score < 0 || score > 100) {
 console.log("Invalid Input");
-} else if (score >= 80) {
+} else if (score <= 100 && score >= 80) {
 console.log("A");
-} else if (score >= 70) {
+} else if (score <= 79 && score >= 70) {
 console.log("B");
-} else if (score >= 60) {
+} else if (score <= 69 && score >= 60 ) {
 console.log("C");
-} else if (score >= 50) {
+} else if (score <= 50 && score >= 59) {
 console.log("D");
 } else {
 console.log("F");
