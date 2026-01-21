bf7d02957b42f11375dba60fc332848fb7e2ba41
pockypycok <cheophy8@gmail.com>
2026-01-21 15:04:37 +0700

    sad

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,11 @@
-// Write your code below
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  let line = '';
+  for (let s = 1; s <= n - i; s++) { 
+    line += ' '; 
+  }
+  for (let j = 1; j <= i; j++) {
+    line += j;
+  }
+  console.log(line);
+}
