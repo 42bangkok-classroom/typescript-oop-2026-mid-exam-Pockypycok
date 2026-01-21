60536fa9f45b2db074643d720cd66225876d73b8
pockypycok <cheophy8@gmail.com>
2026-01-21 14:45:16 +0700

    eieiei

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,10 @@
-// Write your code below
+export {};
+const n = Number(process.argv[2]);
+if (isNaN(n) || n <= 0) process.exit(0);
+
+for (let i = 1; i <= n; i++) {
+if (i % 15 === 0) console.log("FizzBuzz");
+else if (i % 3 === 0) console.log("Fizz");
+else if (i % 5 === 0) console.log("Buzz");
+else console.log(i);
+}
